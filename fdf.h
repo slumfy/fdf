8a24ca39efe559f915ae10c2dad1be534ed6c4ad
Robert VALENTIN <rvalenti@e1r6p4.42.fr>
2019-02-08 10:28:30 +0100

    altitude en cours

--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: rvalenti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/29 16:15:30 by rvalenti          #+#    #+#             */
-/*   Updated: 2019/02/08 07:55:36 by rvalenti         ###   ########.fr       */
+/*   Updated: 2019/02/08 10:27:46 by rvalenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,6 +61,7 @@ typedef struct	s_data
 	int		y_max;
 	int		gap;
 	int		proj;
+	int		n;
 	t_lst	*list;
 	int		**m;
 	t_point	p;
