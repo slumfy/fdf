3f928c37b045f098e26a488c1640bd916906387f
Robert VALENTIN <rvalenti@e1r6p4.42.fr>
2019-02-08 02:11:37 +0100

    norm

--- draw_map.c
+++ draw_map.c
@@ -6,7 +6,7 @@
 /*   By: rvalenti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/07 04:44:08 by rvalenti          #+#    #+#             */
-/*   Updated: 2019/02/08 00:14:10 by rvalenti         ###   ########.fr       */
+/*   Updated: 2019/02/08 02:09:41 by rvalenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,18 +26,12 @@ void	draw_map(t_data *data, int larg, int haut, int trig)
 		{
 			if (x + 1 < data->x_max)
 			{
-				if (trig)
-					iso(data, x, y, x + 1, y);
-				else
-					parallel(data, x, y, x + 1, y);
+				proj(data, x, y, x + 1, y, trig);
 				line(data, larg, haut);
 			}
 			if (y + 1 < data->y_max)
 			{
-				if (trig)
-					iso(data, x, y, x, y + 1);
-				else
-					parallel(data, x, y, x, y + 1);
+				proj(data, x, y, x, y + 1, trig);
 				line(data, larg, haut);
 			}
 			x++;
@@ -46,7 +40,7 @@ void	draw_map(t_data *data, int larg, int haut, int trig)
 	}
 }
 
-int		iso(t_data *data, int x, int y, int x1, int y1)
+int		proj(t_data *data, int x, int y, int x1, int y1, int trig)
 {
 	int	gap;
 	int ox;
@@ -59,29 +53,19 @@ int		iso(t_data *data, int x, int y, int x1, int y1)
 	oy = y - data->y_max / 2;
 	oy1 = y1 - data->y_max / 2;
 	gap = data->gap;
-	data->p.x = ((ox - oy) * cos(0.523599)) * gap;
-	data->p.y = (-data->map[y][x] + (ox + oy) * sin(0.523599)) * gap;
-	data->p.x1 = ((ox1 - oy1) * cos(0.523599)) * gap;
-	data->p.y1 = (-data->map[y1][x1] + (ox1 + oy1) * sin(0.523599)) * gap;
-	return (0);
-}
-
-int		parallel(t_data *data, int x, int y, int x1, int y1)
-{
-	int	gap;
-	int ox;
-	int ox1;
-	int oy;
-	int oy1;
-
-	ox = x - data->x_max / 2;
-	ox1 = x1 - data->x_max / 2;
-	oy = y - data->y_max / 2;
-	oy1 = y1 - data->y_max / 2;
-	gap = data->gap;
-	data->p.x = (ox - data->map[y][x] * cos(0.785398)) * gap;
-	data->p.y = (oy - data->map[y][x] * sin(0.785398)) * gap;
-	data->p.x1 = (ox1 - data->map[y1][x1] * cos(0.785398)) * gap;
-	data->p.y1 = (oy1 - data->map[y1][x1] * sin(0.785398)) * gap;
+	if (trig)
+	{
+		data->p.x = ((ox - oy) * cos(0.523599)) * gap;
+		data->p.y = (-data->map[y][x] + (ox + oy) * sin(0.523599)) * gap;
+		data->p.x1 = ((ox1 - oy1) * cos(0.523599)) * gap;
+		data->p.y1 = (-data->map[y1][x1] + (ox1 + oy1) * sin(0.523599)) * gap;
+	}
+	else
+	{
+		data->p.x = (ox - data->map[y][x] * cos(0.785398)) * gap;
+		data->p.y = (oy - data->map[y][x] * sin(0.785398)) * gap;
+		data->p.x1 = (ox1 - data->map[y1][x1] * cos(0.785398)) * gap;
+		data->p.y1 = (oy1 - data->map[y1][x1] * sin(0.785398)) * gap;
+	}
 	return (0);
 }
